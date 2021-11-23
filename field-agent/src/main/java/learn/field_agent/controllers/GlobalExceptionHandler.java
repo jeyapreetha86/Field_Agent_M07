9c40ecec95152f3994e77f920bb3a066a00824b4
Jeya Preetha Ganesh Kumar <jkumar@dev-10.com>
2021-11-22 18:30:40 -0600

    Changes

--- field-agent/src/main/java/learn/field_agent/controllers/GlobalExceptionHandler.java
+++ field-agent/src/main/java/learn/field_agent/controllers/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package learn.field_agent.controllers;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -38,6 +40,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleException(Exception ex) {
 
         // Log the exception?
+
+        if (ex instanceof HttpMessageNotReadableException || ex instanceof HttpMediaTypeNotSupportedException) {
+            return new ResponseEntity<ErrorResponse>(
+                    new ErrorResponse("Data format not supported :("),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
         return new ResponseEntity<ErrorResponse>(
                 new ErrorResponse("Something went wrong on our end. Your request failed. :("),
